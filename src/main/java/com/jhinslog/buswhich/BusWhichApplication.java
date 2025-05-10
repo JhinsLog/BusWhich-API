5ba4ac819590f0a676aae419413c8aca45003a91
Jhin <joojh501@gmail.com>
2025-05-10 23:58:01 +0900

    Init Commit

---
+++ src/main/java/com/jhinslog/buswhich/BusWhichApplication.java
@@ -0,0 +1,13 @@
+package com.jhinslog.buswhich;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusWhichApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusWhichApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jhinslog/buswhich/BusWhichApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhinslog.buswhich;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusWhichApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
